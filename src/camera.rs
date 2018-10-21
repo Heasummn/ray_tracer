21f7a64a5da643bf60fe77a6bd1e856ae57c1b88
Heasummn <hassamuddin20@gmail.com>
2018-10-20 23:30:13 -0500

    incredibly basic ray intersection. TODO: Seems to be a bug if origin is not 0,0

--- src/camera.rs
+++ src/camera.rs
@@ -15,20 +15,20 @@ impl Camera {
       width: width,
       height: height,
       fov: fov,
-      aspect_ratio: width as f64 / height as f64,
+      aspect_ratio: (width as f64) / (height as f64),
     };
   }
 
   // Makes use of basic trig to convert x/y to camera coords for ray
-  pub fn get_ray(self, x: u32, y: u32) -> Ray {
+  pub fn get_ray(&self, x: u32, y: u32) -> Ray {
     let fov_correction: f64 = (self.fov.to_radians() / 2.0).tan();
     return Ray {
       origin: Vec3::zeros(),
       direction: Vec3 {
-        x: (2.0 * ((x as f64 + 0.5) / self.width as f64) - 1.0) // converting (0, width) -> (-1, 1)
+        x: (2.0 * ((x as f64 + 0.5) / (self.width as f64)) - 1.0) // converting (0, width) -> (-1, 1)
           * fov_correction
           * self.aspect_ratio, // correcting square size for non-square images
-        y: (1.0 - 2.0 * (y as f64 + 0.5) / self.height as f64),
+        y: (1.0 - 2.0 * ((y as f64 + 0.5) / (self.height as f64))) * fov_correction,
         z: -1.0,
       }
       .as_unit(),
--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,46 @@ extern crate image;
 
 mod camera;
 mod ray;
+mod sphere;
 mod vec3;
 
 fn main() {
-    let width: u32 = 1600;
+    let width: u32 = 800;
     let height: u32 = 800;
     let mut dynamic_image = image::DynamicImage::new_rgb8(width, height);
     let imgbuf = dynamic_image.as_mut_rgb8().unwrap();
 
-    let camera = camera::Camera::new(2, 1, 90.0);
+    let camera = camera::Camera::new(width, height, 90.0);
+    let sphere = sphere::Sphere {
+        position: vec3::Vec3 {
+            x: 0.0,
+            y: 0.0,
+            z: -5.0,
+        },
+        radius: 1.0,
+    };
 
-    let ray = camera.get_ray(0, 0);
+    for x in 0..camera.width {
+        for y in 0..camera.height {
+            let ray = camera.get_ray(x, y);
+            if sphere.intersects(&ray) {
+                imgbuf.put_pixel(x, y, image::Rgb([255, 0, 0]))
+            } else {
+                imgbuf.put_pixel(x, y, image::Rgb([0, 0, 0]))
+            }
+        }
+    }
 
-    println!("direction: {:?}", ray.direction);
-
-    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+    /*for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
         let data = vec3::Vec3 {
             x: (x as f64 / width as f64),
             y: ((height - y) as f64) / height as f64,
             z: 0.2,
         } * 255.0;
-
+    
         //println!("{:#?}", data);
         *pixel = data.into();
-    }
+    }*/
 
     imgbuf.save("output.png").unwrap();
 }
