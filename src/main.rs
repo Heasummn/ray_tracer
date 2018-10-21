88443328c3424fd7cf7a9c5096132eff05935124
Heasummn <hassamuddin20@gmail.com>
2018-10-20 21:38:51 -0500

    working camera?

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate image;
 
+mod camera;
+mod ray;
 mod vec3;
 
 fn main() {
@@ -7,6 +9,12 @@ fn main() {
     let height: u32 = 800;
     let mut dynamic_image = image::DynamicImage::new_rgb8(width, height);
     let imgbuf = dynamic_image.as_mut_rgb8().unwrap();
+
+    let camera = camera::Camera::new(2, 1, 90.0);
+
+    let ray = camera.get_ray(0, 0);
+
+    println!("direction: {:?}", ray.direction);
 
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
         let data = vec3::Vec3 {
